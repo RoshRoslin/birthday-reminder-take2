f05355a7804a2d8eaf8720a0fb539e366324b793
RoshRoslin <RoshRoslin@gmail.com>
2021-05-05 19:24:58 -0700

    Updates

---
+++ precache-manifest.a7da7f6b43a4b45b70ee954076d5624a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abebc58eae11bcfa708a247cbc264223",
+    "url": "/birthday-reminder-take2/index.html"
+  },
+  {
+    "revision": "b9a343ce6cfa9fffab36",
+    "url": "/birthday-reminder-take2/static/css/main.a14160c6.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/birthday-reminder-take2/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-reminder-take2/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9a343ce6cfa9fffab36",
+    "url": "/birthday-reminder-take2/static/js/main.7ee3c885.chunk.js"
+  },
+  {
+    "revision": "3d839e31c078dffa04cb",
+    "url": "/birthday-reminder-take2/static/js/runtime-main.56a1c977.js"
+  }
+]);
